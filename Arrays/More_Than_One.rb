fbe75161284aec6c7db95342775fff90df2f6115
Nick <nhopkin@jacksonville.edu>
2019-07-22 22:33:00 -0400

    arrays exercises

---
+++ Arrays/More_Than_One.rb
@@ -0,0 +1,3 @@
+pets = ['cat', 'dog', 'fish', 'lizard']
+my_pets = pets
+puts "I have a pet #{my_pets.fetch(2)} and a #{my_pets.fetch(3)}"
---
+++ Arrays/What_Color_Are_You.rb
@@ -0,0 +1,3 @@
+colors = ['red', 'yellow', 'purple', 'green']
+
+colors.each {|n| puts "I'm the color #{n}!"}
